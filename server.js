bd80fc66fbc2bd29dc1a08db01d9024f81883664
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-07-25 03:08:30 +1000

    feat: GET request to fetch repositories

--- server.js
+++ server.js
@@ -30,3 +30,29 @@ app.get('/getAccessToken', async function (request, response) {
         console.error("Error fetching access token: ", error);
     });
 });
+
+app.get('getUserRepos', async function (request, response) {
+    const authHeader = request.get("Authorization");
+
+    if (!authHeader) {
+        return response.status(401).json({ error: "Authorization header missing" });
+    }
+
+    await fetch("https://api.github.com/user/repos", {
+        method: "GET",
+        headers: {
+            "Authorization": authHeader
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        response.json(data);
+    }).catch((error) => {
+        console.error("Error fetching user repositories: ", error);
+        response.status(500).json({ error: "Internal server error" });
+    });
+});
+
+app.listen(4000, function () {
+    console.log("CORS server running on port 4000");
+});
